405255505d06d500886143286b89b38f3559efa1
MadGriffon <madgriffon.games@gmail.com>
2018-03-20 18:36:50 +0300

    тмп

---
+++ Assets/Scripts/EasyGameEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasyGameEvent : IGameEvent
+{
+    const int SPEED_RANGE_BEGIN = 1;
+    const int SPEED_RANGE_END = 6;
+
+    const int ELEMENTS_RANGE_BEGIN = 1;
+    const int ELEMENTS_RANGE_END = 6;
+
+    public void Execute()
+    {
+
+    }
+}
