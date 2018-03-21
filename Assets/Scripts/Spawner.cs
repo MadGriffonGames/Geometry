7d2eea80bf118b8eae20cc2f80593eac77a2b01f
Chataev Gleb <chataev.gs@gmail.com>
2018-03-21 13:56:08 +0300

    tmp

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 
+	[SerializeField]
+	GameObject figure;
+
 	private float radius = 50f;
 	public Element newElement;
+
+
 
 	void SpawnElement()
 	{
@@ -16,7 +21,11 @@ public class Spawner : MonoBehaviour {
 
 	Vector3 generatePoint()
 	{
-		return new Vector3 (0, 0, 0);
+		Vector3 newPoint = new Vector3 (0, 0, 0);;
+		int degree = UnityEngine.Random.Range (0, 360);
+		newPoint.x = figure.transform.position.x + radius * Mathf.Cos(degree);
+		newPoint.y = figure.transform.position.y + radius * Mathf.Cos(degree);
+		return newPoint;
 	}
 
 	Color ChooseColor()
